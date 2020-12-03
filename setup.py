1870713946b6ce4b87277c799b09db208c748d75
dmarienburg <dmarienburg@pps.net>
2020-12-03 14:14:56 -0800

    fixes to allow twine push to pypi

--- setup.py
+++ setup.py
@@ -11,12 +11,13 @@ setuptools.setup(
     author_email="david.marienburg@gmail.com",
     description="A simple script for automating your downloads of data from the Clever sftp site",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/dmarienburg/CleverFetch",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Langauge :: Python :: 3",
-        "License :: MIT License",
-        "Operating System :: OS Independent"
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires=">=3.0"
 )
